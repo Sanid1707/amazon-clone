34fd00afd7e4b8d340ab75f4f84bbdc408f88a76
Sanid1707 <pandeysanid@gmail.com>
2023-07-26 05:08:20 +0100

    navbar completed ,with the cart done as well

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,24 +1,15 @@
 import "./App.css";
+import React from "react";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div className="app">
+      {/* using bem convention , which is a reall7y good way to name your css */}
 
-      <h1>Hello world </h1>
+      <Header />
+      {/* {header} */}
+      {/* {home} */}
     </div>
   );
 }
