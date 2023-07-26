9de60c8647550b7f3bf0abf10be92271f1fb2c3b
Sanid1707 <pandeysanid@gmail.com>
2023-07-26 05:05:31 +0100

    navbar done except the cart is still black

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,8 +1,8 @@
 // all the code in the header component is going to be rendered in the header component
-
 import React from "react";
 import "./Header.css";
-import SavedSearchIcon from "@mui/icons-material/SavedSearch";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
@@ -15,25 +15,23 @@ function Header() {
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
-        {/* logo */}
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineOne">Hello Guest </span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
 
         <div className="header__option">
-          <span className="header__optionLineOne">Seach</span>
-          import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+          <span className="header__optionLineOne">Returns </span>
+          <span className="header__optionLineTwo">&Orders</span>
         </div>
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Orders</span>
-        </div>
-
-        <div className="header__option">
-          <span className="header__optionLineOne">Cart</span>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
