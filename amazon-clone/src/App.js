bd58dcfdc44fc05bfef1de952cd6b82865031eb7
Sanid1707 <pandeysanid@gmail.com>
2023-07-31 03:22:57 +0100

    tried using bootstrap components

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,21 +5,28 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 // import data from "./data";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <BrowserRouter>
+      <div className="d-flex flex-column site-container"></div>
       <div className="app">
         {/* using bem convention , which is a reall7y good way to name your css */}
 
         <Header />
 
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved.</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
