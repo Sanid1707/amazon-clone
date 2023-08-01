ef3c0b83caebd292ee9039b85709adab88e1ba7c
Sanid1707 <pandeysanid@gmail.com>
2023-08-01 03:00:10 +0100

    finished with products page

--- amazon-clone/src/screens/HomeScreen.js
+++ amazon-clone/src/screens/HomeScreen.js
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import logger from "use-reducer-logger"; // this is a middleware for reducer function
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Products";
+import { Helmet } from "react-helmet-async";
 
 const reducer = (state, action) => {
   // creating a reducer function
@@ -43,6 +47,9 @@ function HomeScreen() {
 
   return (
     <div>
+      <Helmet>
+        <title>amazona</title>
+      </Helmet>
       <h1>Featured products</h1>
 
       <div className="products">
@@ -53,22 +60,13 @@ function HomeScreen() {
           ) : error ? (
             <div>{error}</div>
           ) : (
-            products.map((product) => (
-              <div className="product" key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>add to cart</button>
-                </div>
-              </div>
-            ))
+            <Row>
+              {products.map((product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                  <Product product={product}></Product>
+                </Col>
+              ))}
+            </Row>
           )
         }
       </div>
