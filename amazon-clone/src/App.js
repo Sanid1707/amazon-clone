5d6f3be2482275d890cf9c982c1af4a28afc6511
Sanid1707 <pandeysanid@gmail.com>
2023-10-30 20:16:00 +0000

    Added functionality to Cart Screen

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -12,6 +12,7 @@ import Badge from "react-bootstrap/Badge";
 import Nav from "react-bootstrap/Nav";
 import { useContext } from "react";
 import { Store } from "./Store";
+import CartScreen from "./screens/CartScreen";
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -33,7 +34,7 @@ function App() {
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                      {cart.cartItems.length}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
@@ -46,6 +47,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
+              <Route path="/Cart" element={<CartScreen />} />
             </Routes>
           </Container>
         </main>
--- amazon-clone/src/screens/ProductScreen.js
+++ amazon-clone/src/screens/ProductScreen.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { useContext, useEffect, useReducer } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Rating from "../components/Rating";
 import ListGroup from "react-bootstrap/ListGroup";
 import Badge from "react-bootstrap/Badge";
-import Button from "react-bootstrap/esm/Button";
+import Button from "react-bootstrap/Button";
 import { Helmet } from "react-helmet-async";
 import { Store } from "../Store";
 
@@ -26,6 +26,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate(); //  for the cart
   const params = useParams();
   const { slug } = params;
 
@@ -50,11 +51,21 @@ function ProductScreen() {
   }, [slug]);
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const addToCartHandler = () => {
+  const { cart } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry. Product is out of stock");
+      return;
+    }
     ctxDispatch({
       type: "CART_ADD_ITEM",
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
+
+    navigate("/cart");
   };
 
   return loading ? (
