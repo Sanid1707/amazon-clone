0c30293c3168d451db5b395129900a216de88c37
Sanid1707 <pandeysanid@gmail.com>
2023-10-30 21:17:04 +0000

    Brought the fields  in the middle

--- amazon-clone/src/screens/SigninScreen.js
+++ amazon-clone/src/screens/SigninScreen.js
@@ -5,41 +5,62 @@ import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+
+import backgroundImage from "../background.jpg"; // Import your background image
+
+const backgroundStyle = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  minHeight: "100vh",
+  display: "flex",
+  alignItems: "center",
+};
 
 export default function SigninScreen() {
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
   const redirect = redirectInUrl ? redirectInUrl : "/";
-  return (
-    <Container className="small-container">
-      <Helmet>
-        <title>Sign In | Amazon</title>
-      </Helmet>
-      <h1 classname="mb-3">Sign In</h1>
-      <Form>
-        <Form.Group classname="mb-3" controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" required />
-        </Form.Group>
 
-        <Form.Group classname="mb-3" controlId="password">
-          <Form.Label>password</Form.Label>
-          <Form.Control type="password" required />
-        </Form.Group>
-        <div className="mb-3">
-          <Button type="submit" variant="primary">
-            Sign In
-          </Button>
-        </div>
-        {/* this is the link to the signup page */}
-        <div className="mb-3">
-          <Button type="submit">
-            New Customer?{""}
+  return (
+    <div style={backgroundStyle}>
+      <Container className="small-container">
+        <Helmet>
+          <title>Sign In | Amazon</title>
+        </Helmet>
+        <h1 className="mb-3">Sign In</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              required
+            />
+          </Form.Group>
+
+          <div className="mb-3">
+            <Button type="submit" variant="primary" block>
+              Sign In
+            </Button>
+          </div>
+
+          {/* Link to the signup page */}
+          <div className="mb-3 text-center">
+            New Customer?{" "}
             <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
-          </Button>
-        </div>
-      </Form>
-    </Container>
+          </div>
+        </Form>
+      </Container>
+    </div>
   );
 }
