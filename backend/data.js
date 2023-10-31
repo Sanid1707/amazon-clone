9c4d8e3f85014ef0c067a6a24b24cfb94e1ec1ed
Sanid1707 <pandeysanid@gmail.com>
2023-10-31 01:42:00 +0000

    Created Users Schema

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Radhika",
+      email: "pandeysanid@gmail.com",
+      password: bcrypt.hashSync("1234"),
+      isAdmin: true,
+    },
+    {
+      name: "John",
+      email: "sanid@ggg.com",
+      password: bcrypt.hashSync("1234"),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       // _id: "1",
