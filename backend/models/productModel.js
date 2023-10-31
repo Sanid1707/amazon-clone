8224d9fa3570916f2e050de6eda74f190f3ffbc8
Sanid1707 <pandeysanid@gmail.com>
2023-10-31 01:28:22 +0000

    Created Routes for product and seedRoute

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -5,9 +5,9 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true, unique: true },
     slug: { type: String, required: true, unique: true },
     image: { type: String, required: true },
+    brand: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
-    brand: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
@@ -15,10 +15,8 @@ const productSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-    // Mongoose will create a createdAt and updatedAt properties to schema
   }
 );
 
 const Product = mongoose.model("Product", productSchema);
-
 export default Product;
